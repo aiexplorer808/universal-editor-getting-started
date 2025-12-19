3f956a55c607c27c8be6ba54d27778655a654168
Pushpendu Arunkumar Samanta <Pushpendu.Arunkumar.Samanta+EYGS@gds.ey.com>
2025-12-19 18:28:40 +0530

    code 4

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,16 +1,16 @@
 export default function decorate(block) {
   const firstRow = block.querySelector(':scope > div:first-child');
-  console.log('firstRow?', !!firstRow);
+
   if (!firstRow) return;
 
   const videoLink = firstRow.querySelector('a[href$=".mp4"]');
-  console.log('videoLink?', !!videoLink, 'href:', videoLink?.getAttribute('href'));
+
   if (!videoLink) return;
 
   const video = document.createElement('video');
   video.src = videoLink.href;
-  console.log('video.src:', video.src);
 
+  
   video.autoplay = true;
   video.muted = true;
   video.loop = true;

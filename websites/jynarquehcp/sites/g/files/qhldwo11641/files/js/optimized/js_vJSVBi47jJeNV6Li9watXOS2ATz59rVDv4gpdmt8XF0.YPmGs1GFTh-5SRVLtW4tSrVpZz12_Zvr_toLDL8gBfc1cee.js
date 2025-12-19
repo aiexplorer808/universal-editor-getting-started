47982ce30cd787b7168fe021cc9e37c9e7d06ca6
Pushpendu Arunkumar Samanta <Pushpendu.Arunkumar.Samanta+EYGS@gds.ey.com>
2025-12-19 13:48:27 +0530

    code clean up

--- websites/jynarquehcp/sites/g/files/qhldwo11641/files/js/optimized/js_vJSVBi47jJeNV6Li9watXOS2ATz59rVDv4gpdmt8XF0.YPmGs1GFTh-5SRVLtW4tSrVpZz12_Zvr_toLDL8gBfc1cee.js
+++
@@ -1,8 +0,0 @@
-(function ($, Drupal) {
-  'use strict';
-  Drupal.behaviors.otsukaParagraphsWrapperTabbedParagraph = {
-    attach: function (context) {
-      $('.custom-wrapper-type-tabbed > .field--name-field-paragraphs', context).tabs();
-    }
-  };
-})(jQuery, Drupal);
